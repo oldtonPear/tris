1a10cc8fdc4410bc08d593901551ca4b281fe66e
fenicemaster <116422802+PyroGabrielKotzell@users.noreply.github.com>
2024-05-02 17:52:32 +0200

    aggiunta immagini -da fare codice-
    migliorata la gestione dei pulsanti

--- GUI/Tris_Board.java
+++ GUI/Tris_Board.java
@@ -1,28 +1,11 @@
-import java.util.LinkedList;
-
 /**
  *
  * @author Gabriele Urban
  */
 public class Tris_Board extends javax.swing.JPanel implements Observer{
-    private javax.swing.JButton button1;
-    private javax.swing.JButton button2;
-    private javax.swing.JButton button3;
-    private javax.swing.JButton button4;
-    private javax.swing.JButton button5;
-    private javax.swing.JButton button6;
-    private javax.swing.JButton button7;
-    private javax.swing.JButton button8;
-    private javax.swing.JButton button9;
-    private javax.swing.JPanel cell1;
-    private javax.swing.JPanel cell2;
-    private javax.swing.JPanel cell3;
-    private javax.swing.JPanel cell4;
-    private javax.swing.JPanel cell5;
-    private javax.swing.JPanel cell6;
-    private javax.swing.JPanel cell7;
-    private javax.swing.JPanel cell8;
-    private javax.swing.JPanel cell9;
+    private javax.swing.JPanel[] cells;
+    private javax.swing.JButton[] buttons;
+    
     private boolean myTurn;
     private Client client;
     private Shared_board sh = new Shared_board();
@@ -36,282 +19,48 @@ public class Tris_Board extends javax.swing.JPanel implements Observer{
     @SuppressWarnings("unchecked")
     private void initComponents() {
 
-        cell1 = new javax.swing.JPanel();
-        button1 = new javax.swing.JButton();
-        cell2 = new javax.swing.JPanel();
-        button2 = new javax.swing.JButton();
-        cell3 = new javax.swing.JPanel();
-        button3 = new javax.swing.JButton();
-        cell4 = new javax.swing.JPanel();
-        button4 = new javax.swing.JButton();
-        cell5 = new javax.swing.JPanel();
-        button5 = new javax.swing.JButton();
-        cell6 = new javax.swing.JPanel();
-        button6 = new javax.swing.JButton();
-        cell7 = new javax.swing.JPanel();
-        button7 = new javax.swing.JButton();
-        cell8 = new javax.swing.JPanel();
-        button8 = new javax.swing.JButton();
-        cell9 = new javax.swing.JPanel();
-        button9 = new javax.swing.JButton();
+        cells = new javax.swing.JPanel[9];
+        for (int i = 0; i < cells.length; i++) cells[i] = new javax.swing.JPanel();
+        
+        buttons = new javax.swing.JButton[9];
+        for (int i = 0; i < cells.length; i++) buttons[i] = new javax.swing.JButton();
 
         setPreferredSize(new java.awt.Dimension(390, 235));
         setLayout(new java.awt.GridLayout(3, 3));
 
-        cell1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        for (int i = 0; i < cells.length; i++) {
+            cells[i].setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+            buttons[i].setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+            buttons[i].setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+            buttons[i].setOpaque(false);
+            
+            final int t = i;
+            buttons[i].addActionListener(new java.awt.event.ActionListener() {
+                
+                public void actionPerformed(java.awt.event.ActionEvent evt) {
+                    buttonActionPerformed(evt, t);
+                }
+            });
 
-        button1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        button1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        button1.setOpaque(false);
-        button1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                button1ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout cell1Layout = new javax.swing.GroupLayout(cell1);
-        cell1.setLayout(cell1Layout);
-        cell1Layout.setHorizontalGroup(
-            cell1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button1, javax.swing.GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE)
-        );
-        cell1Layout.setVerticalGroup(
-            cell1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button1, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
-        );
-
-        button1.getAccessibleContext().setAccessibleDescription("");
-
-        add(cell1);
-
-        cell2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        button2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        button2.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        button2.setOpaque(false);
-        button2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                button2ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout cell2Layout = new javax.swing.GroupLayout(cell2);
-        cell2.setLayout(cell2Layout);
-        cell2Layout.setHorizontalGroup(
-            cell2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button2, javax.swing.GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE)
-        );
-        cell2Layout.setVerticalGroup(
-            cell2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button2, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
-        );
-
-        add(cell2);
-
-        cell3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        button3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        button3.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        button3.setOpaque(false);
-        button3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                button3ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout cell3Layout = new javax.swing.GroupLayout(cell3);
-        cell3.setLayout(cell3Layout);
-        cell3Layout.setHorizontalGroup(
-            cell3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button3, javax.swing.GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE)
-        );
-        cell3Layout.setVerticalGroup(
-            cell3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button3, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
-        );
-
-        add(cell3);
-
-        cell4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        button4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        button4.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        button4.setOpaque(false);
-        button4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                button4ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout cell4Layout = new javax.swing.GroupLayout(cell4);
-        cell4.setLayout(cell4Layout);
-        cell4Layout.setHorizontalGroup(
-            cell4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button4, javax.swing.GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE)
-        );
-        cell4Layout.setVerticalGroup(
-            cell4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button4, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
-        );
-
-        add(cell4);
-
-        cell5.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        button5.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        button5.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        button5.setOpaque(false);
-        button5.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                button5ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout cell5Layout = new javax.swing.GroupLayout(cell5);
-        cell5.setLayout(cell5Layout);
-        cell5Layout.setHorizontalGroup(
-            cell5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button5, javax.swing.GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE)
-        );
-        cell5Layout.setVerticalGroup(
-            cell5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button5, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
-        );
-
-        add(cell5);
-
-        cell6.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        button6.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        button6.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        button6.setOpaque(false);
-        button6.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                button6ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout cell6Layout = new javax.swing.GroupLayout(cell6);
-        cell6.setLayout(cell6Layout);
-        cell6Layout.setHorizontalGroup(
-            cell6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button6, javax.swing.GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE)
-        );
-        cell6Layout.setVerticalGroup(
-            cell6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button6, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
-        );
-
-        add(cell6);
-
-        cell7.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        button7.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        button7.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        button7.setOpaque(false);
-        button7.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                button7ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout cell7Layout = new javax.swing.GroupLayout(cell7);
-        cell7.setLayout(cell7Layout);
-        cell7Layout.setHorizontalGroup(
-            cell7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button7, javax.swing.GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE)
-        );
-        cell7Layout.setVerticalGroup(
-            cell7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button7, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
-        );
-
-        add(cell7);
-
-        cell8.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        button8.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        button8.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        button8.setOpaque(false);
-        button8.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                button8ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout cell8Layout = new javax.swing.GroupLayout(cell8);
-        cell8.setLayout(cell8Layout);
-        cell8Layout.setHorizontalGroup(
-            cell8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button8, javax.swing.GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE)
-        );
-        cell8Layout.setVerticalGroup(
-            cell8Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button8, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
-        );
-
-        add(cell8);
-
-        cell9.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        button9.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        button9.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        button9.setOpaque(false);
-        button9.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                button9ActionPerformed(evt);
-            }
-        });
-
-        javax.swing.GroupLayout cell9Layout = new javax.swing.GroupLayout(cell9);
-        cell9.setLayout(cell9Layout);
-        cell9Layout.setHorizontalGroup(
-            cell9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button9, javax.swing.GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE)
-        );
-        cell9Layout.setVerticalGroup(
-            cell9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(button9, javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
-        );
-
-        add(cell9);
+            javax.swing.GroupLayout cellLayout = new javax.swing.GroupLayout(cells[i]);
+            cells[i].setLayout(cellLayout);
+            cellLayout.setHorizontalGroup(
+                cellLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(buttons[i], javax.swing.GroupLayout.DEFAULT_SIZE, 198, Short.MAX_VALUE)
+            );
+            cellLayout.setVerticalGroup(
+                cellLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addComponent(buttons[i], javax.swing.GroupLayout.DEFAULT_SIZE, 131, Short.MAX_VALUE)
+            );
+            buttons[i].getAccessibleContext().setAccessibleDescription("");
+            add(cells[i]);
+        }
     }
 
-    private void button1ActionPerformed(java.awt.event.ActionEvent evt) {
-        doAction(1);
-    }
-    
-    private void button2ActionPerformed(java.awt.event.ActionEvent evt) {
-        doAction(2);
+    private void buttonActionPerformed(java.awt.event.ActionEvent evt, int i) {
+        doAction(i);
     }
 
-    private void button3ActionPerformed(java.awt.event.ActionEvent evt) {
-        doAction(3);
-    }
-
-    private void button4ActionPerformed(java.awt.event.ActionEvent evt) {
-        doAction(4);
-    }
-
-    private void button5ActionPerformed(java.awt.event.ActionEvent evt) {
-        doAction(5);
-    }
-
-    private void button6ActionPerformed(java.awt.event.ActionEvent evt) {
-        doAction(6);
-    }
-
-    private void button7ActionPerformed(java.awt.event.ActionEvent evt) {
-        doAction(7);
-    }
-
-    private void button8ActionPerformed(java.awt.event.ActionEvent evt) {
-        doAction(8);
-    }
-
-    private void button9ActionPerformed(java.awt.event.ActionEvent evt) {
-        doAction(9);
-    }
     private void doAction(int n){ 
         if(myTurn){
             sh.setBoard(sh.getBoard() + "a");
